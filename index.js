6877229dc58205d149d59ae911345dac11ae02ed
shayan <sak704@nyu.edu>
2022-02-06 14:09:16 -0800

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+
+const currentUser = 'Shayan Khan';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+
